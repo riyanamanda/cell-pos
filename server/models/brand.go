a500c990966cee7928d23af89135af39f87f1d8b
Riyan Amanda <ryant.n92@gmail.com>
2023-11-06 21:25:02 +0700

    refactor: remove gorm model on brand

--- server/models/brand.go
+++ server/models/brand.go
@@ -1,19 +1,10 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
-type Model struct {
-	ID        uint           `gorm:"primaryKey"`
-	CreatedAt time.Time		 `json:"created_at"`
-	UpdatedAt time.Time		 `json:"updated_at"`
-  }
+import "time"
 
 type Brand struct {
-	ID        int    `gorm:"primaryKey;type:smallint" json:"id"`
-	Name      string `gorm:"type:varchar(100)" json:"name"`
-	gorm.Model
+	ID        uint   	`gorm:"primaryKey" json:"id"`
+	Name      string 	`gorm:"type:varchar(100)" json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
